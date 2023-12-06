b4b56eb1f8259142bb400fea454704eb88fe6e0f
psychotr0pia <alejandro.villalobos@alumnos.uv.cl>
2023-12-06 11:01:07 -0300

    certamen 3 pregunta 1 y 2 listas

--- certamen3/quitarStopWords.py
+++ certamen3/quitarStopWords.py
@@ -1,19 +1,30 @@
-#funcion para quitar las stopwords de forma recursiva
-def quitarStopwords(paginasWeb, listaStopwords):
 
-    #si la stopword no esta en el texto, la quitamos de la lista
-    if listaStopwords and listaStopwords[0] not in paginasWeb:
-        #quitar stopword
-        listaStopwordsActualizada = listaStopwords[1:]
-        #llamamos la funcion de nuevo
-        return quitarStopwords(paginasWeb, listaStopwordsActualizada)
-    elif not listaStopwords: #lista vacia, no hay mas stopwords que sacar!
-        return paginasWeb
-    else: #si la stopword esta en el texto, la quitamos del texto
-        stopword = listaStopwords[0]
-        print(f"Quitando stopword {stopword}")
-        paginasWebActualizada = paginasWeb.replace(stopword, '') #la quitamos y la reemplazamos por ''
-        return quitarStopwords(paginasWebActualizada, listaStopwords)
+#funcion para quitar las stopwords de forma recursiva
+def quitarStopwords(texto, listaStopwords):
+    # Dividemos el texto en lineas
+    lineas = texto.split('\n')
+
+    # BUscamos en cada linea
+    for i in range(len(lineas)):
+        # Limpieza
+        linea = lineas[i].strip()
+
+        # checkamos si el stopword esta en las lineas
+        for stopword in listaStopwords:
+            if linea.startswith(stopword):
+                # si encontramos la stopword, la quitamos de la lista
+                listaStopwords.remove(stopword)
+                #quitamos la palabra de la linea
+                linea = linea[len(stopword):].strip()
+                print(f"quitamos stop word {stopword} de la linea {i}")
+                #volvemos a unir las lienas en un puro texto
+                textoMod = '\n'.join(lineas)
+                #llamada recursiva con el texto modificado
+                quitarStopwords(textoMod, listaStopwords)
+    #si no encontramos mas stopwords, devolvemos el texto
+    texto = '\n'.join(lineas)
+    return texto
+
 
 with open('indexINvertido.txt', 'r', encoding='utf-8') as paginasWeb:
     paginasWeb = paginasWeb.read()
@@ -33,7 +44,6 @@ listaStopwords = stopwords = [
     "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren',
     "weren't", 'won', "won't", 'wouldn', "wouldn't"
 ]
-
 
 paginasWebStopwordnt = quitarStopwords(paginasWeb, listaStopwords)
 with open('indexInvertidoFiltrado.txt', 'w', encoding='utf-8') as indexInvertidoFiltrado:
